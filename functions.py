611be147ac29df066b67dded53b05b99e0f5b2de
jasur8135 <maxammadjonovjasurbek628@gmail.com>
2024-09-22 15:21:09 +0500

    code for learning functions in python

---
+++ functions.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep 22 13:55:27 2024
+
+@author: laptop
+"""
+
+
+
+def sumOfTwoNumbers(firstNumber=4, secondNumber=10):
+    return firstNumber+secondNumber
+
+
+print(sumOfTwoNumbers(10, 20))
+print(sumOfTwoNumbers(firstNumber=23, secondNumber=334))
+print(sumOfTwoNumbers(323,2))
